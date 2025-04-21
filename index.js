8b5f7409f4acecca18e13e20417a0ce9dde05508
Lenka Koubková <lvseteckova@gmail.com>
2025-04-21 10:10:03 +0200

    upravení shoplist.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import { list } from './shoplist.js';
+import { renderList} from "./render.js";
 
-const list = [
+//posíláme do shoplist.js
+/*const list = [
   { name: 'Mléko', amount: '1 litr', bought: true },
   { name: 'Rohlíky', amount: '10 ks', bought: false },
   { name: 'Pivo Krušovice', amount: '2 ks', bought: false },
   { name: 'Sýr Eidem', amount: '200 g', bought: true },
   { name: 'Paštika', amount: '1 ks', bought: false },
   { name: 'Máslo', amount: '250 g', bought: true },
-];
+];*/
 
-const renderList = () => {
+/*const renderList = () => {
   const shoplistElement = document.querySelector('.shoplist');
   shoplistElement.innerHTML = '';
   list.forEach((item) => {
@@ -35,16 +37,16 @@ const renderList = () => {
       list.splice(index, 1);
       renderList();
     });
-  });
+  });*/
 
-  const tickButtons = document.querySelectorAll('.shopitem .btn-tick');
+  /*const tickButtons = document.querySelectorAll('.shopitem .btn-tick');
   tickButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
       list[index].bought = !list[index].bought;
       renderList();
     });
   });
-};
+};*/
 
 const formElement = document.querySelector('.newitem-form');
 formElement.addEventListener('submit', (e) => {
@@ -52,12 +54,7 @@ formElement.addEventListener('submit', (e) => {
 
   const nameInput = document.querySelector('#input-name');
   const amountInput = document.querySelector('#input-amount');
-  const newItem = {
-    name: nameInput.value,
-    amount: amountInput.value,
-    bought: false,
-  };
-  list.push(newItem);
+  addItem(nameInput.value, amountInput.value);
   renderList();
 
   nameInput.value = '';
--- shoplist.js
+++ shoplist.js
@@ -1,6 +1,11 @@
 export const list = [
-  // ...
-];
+  { name: 'Mléko', amount: '1 litr', bought: true },
+  { name: 'Rohlíky', amount: '10 ks', bought: false },
+  { name: 'Pivo Krušovice', amount: '2 ks', bought: false },
+  { name: 'Sýr Eidem', amount: '200 g', bought: true },
+  { name: 'Paštika', amount: '1 ks', bought: false },
+  { name: 'Máslo', amount: '250 g', bought: true },
+]; 
 
 // funkce pro upravování pole s položkami seznamu
 //funkce pro přidání nové položky
